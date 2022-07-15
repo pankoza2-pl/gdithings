ac028e7e5fd614d5a1b806fb7099742bb223e306
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-07-15 13:28:28 +0200

    Create elipse.cpp

---
+++ elipse.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+int main()
+{
+	while(1){
+		HDC hdc = GetDC(0);
+		int x = GetSystemMetrics(0);
+		int y = GetSystemMetrics(1);
+		HBRUSH brush = CreateSolidBrush(RGB(rand() % 255, rand() % 255, rand() % 255));
+		SelectObject(hdc, brush);
+		Pie(hdc, rand() % x, rand() % y, rand() % x, rand() % y, rand() % x, rand() % y, rand() % x, rand() % y);
+    	DeleteObject(brush);
+    	ReleaseDC(NULL, hdc);
+    	Sleep(100);
+	}
+}

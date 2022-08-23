38da6c8a94393a7d1294788f3256ca4f309b4f37
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-08-23 09:21:52 +0200

    Update betterrgbquad.cpp

--- betterrgbquad.cpp
+++ betterrgbquad.cpp
@@ -17,13 +17,10 @@ DWORD WINAPI shader1(LPVOID lpParam) {
         HDC hdcdc = CreateCompatibleDC(desk);
         HBITMAP hbm = CreateBitmap(w, h, 1, 32, data);
         SelectObject(hdcdc, hbm);
-        BitBlt(hdcdc, 0, 0, w, h, desk, 0, 0, SRCCOPY);
-        GetBitmapBits(hbm, w * h * 4, data);
         for (int i = 0; w * h > i; i++) {
 		int x = i % w, y = i / h, t = y ^ y | x;
 		data[i].rgb = x & y & t;
         }
-        SetBitmapBits(hbm, w * h * 4, data);
         BitBlt(desk, 0, 0, w, h, hdcdc, 0, 0, SRCCOPY);
         DeleteObject(hbm);
         DeleteObject(hdcdc);

2a411ee579ed7c2ea3e885d4be1f37a52fe1e0ff
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-08-23 09:07:48 +0200

    Create betterrgbquad.cpp

---
+++ betterrgbquad.cpp
@@ -0,0 +1,42 @@
+#include <windows.h>
+typedef union _RGBQUAD {
+	COLORREF rgb;
+	struct {
+		BYTE b;
+		BYTE g;
+		BYTE r;
+		BYTE unused;
+	};
+} *PRGBQUAD;
+
+DWORD WINAPI shader1(LPVOID lpParam) {
+    int time = GetTickCount();
+    int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+    _RGBQUAD* data = (_RGBQUAD*)VirtualAlloc(0, (w * h + w) * sizeof(_RGBQUAD), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    for (int i = 0;; i++, i %= 3) {
+        HDC desk = GetDC(NULL);
+        HDC hdcdc = CreateCompatibleDC(desk);
+        HBITMAP hbm = CreateBitmap(w, h, 1, 32, data);
+        SelectObject(hdcdc, hbm);
+        BitBlt(hdcdc, 0, 0, w, h, desk, 0, 0, SRCCOPY);
+        GetBitmapBits(hbm, w * h * 4, data);
+        BYTE byte = 0;
+        if ((GetTickCount() - time) > 60000)
+            byte = rand()%0xff;
+        for (int i = 0; w * h > i; i++) {
+				int x = i % w, y = i / h, t = y ^ y | x;
+				data[i].rgb = x & y & t;
+        }
+        SetBitmapBits(hbm, w * h * 4, data);
+        BitBlt(desk, 0, 0, w, h, hdcdc, 0, 0, SRCCOPY);
+        DeleteObject(hbm);
+        DeleteObject(hdcdc);
+        DeleteObject(desk);
+    }
+    return 0;
+} 
+
+int main() {
+    CreateThread(0, 0, shader1, 0, 0, 0);
+    Sleep(-1);
+}

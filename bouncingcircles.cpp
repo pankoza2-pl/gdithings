6bc11b6dfa40875c613f2171f648ec770f116f4f
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-08-31 09:48:25 +0200

    bouncing rainbow circles (no skid)

---
+++ bouncingcircles.cpp
@@ -0,0 +1,46 @@
+#include <windows.h>
+
+int main(){
+	int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	int signX = 1;
+    int signY = 1;
+    int signX1 = 1;
+    int signY1 = 1;
+    int incrementor = 10;
+    int x = 10;
+    int y = 10;
+	while(1){
+		HDC hdc = GetDC(0);
+        x += incrementor * signX;
+        y += incrementor * signY;
+		int top_x = 0 + x;
+        int top_y = 0 + y;
+        int bottom_x = 100 + x;
+        int bottom_y = 100 + y; 
+    	HBRUSH brush = CreateSolidBrush(RGB(rand() % 255, rand() % 255, rand() % 255));
+    	SelectObject(hdc, brush);
+		Ellipse(hdc, top_x, top_y, bottom_x, bottom_y);
+        if (y == GetSystemMetrics(SM_CYSCREEN))
+        {
+                signY = -1;
+        }
+ 
+        if (x == GetSystemMetrics(SM_CXSCREEN))
+        {
+            signX = -1;
+        }
+ 
+        if (y == 0)
+        {
+            signY = 1;
+        }
+ 
+        if (x == 0)
+        {
+            signX = 1;
+        }
+        Sleep(10);
+    	DeleteObject(brush);
+        ReleaseDC(0, hdc);
+	}
+}

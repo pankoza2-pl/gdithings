0b81c05b5aeab7809802f46d3d487fa4dc82f53a
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-09-10 15:18:50 +0200

    Add files via upload

---
+++ train.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int main() {
+    int w = GetSystemMetrics(0);
+    int h = GetSystemMetrics(1);
+    while (1) {
+        HDC hdc = GetDC(0);
+        BitBlt(hdc, -30, 0, w, h, hdc, 0, 0, SRCCOPY);
+        BitBlt(hdc, w - 30, 0, w, h, hdc, 0, 0, SRCCOPY);
+        ReleaseDC(0, hdc);
+        Sleep(10);
+    }
+}

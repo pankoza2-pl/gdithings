cdd30560da47ce75b1afedf6a7f19e9860841f0e
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-09-29 12:16:43 +0200

    Create hslshader.cpp
    credits to GetMbr for the Hue Function

---
+++ hslshader.cpp
@@ -0,0 +1,67 @@
+#include <windows.h>
+#include <math.h>
+typedef union _RGBQUAD {
+	COLORREF rgb;
+	struct {
+		BYTE b;
+		BYTE g;
+		BYTE r;
+		BYTE unused;
+	};
+} *PRGBQUAD;
+int red, green, blue;
+bool ifcolorblue = false, ifblue = false;
+COLORREF Hue(int length) {
+	if (red != length) {
+		red < length; red++;
+		if (ifblue == true) {
+			return RGB(red, 0, length);
+		}
+		else {
+			return RGB(red, 0, 0);
+		}
+	}
+	else {
+		if (green != length) {
+			green < length; green++;
+			return RGB(length, green, 0);
+		}
+		else {
+			if (blue != length) {
+				blue < length; blue++;
+				return RGB(0, length, blue);
+			}
+			else {
+				red = 0; green = 0; blue = 0;
+				ifblue = true;
+			}
+		}
+	}
+}
+DWORD WINAPI shader1(LPVOID lpParam) {
+    int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+    _RGBQUAD* data = (_RGBQUAD*)VirtualAlloc(0, (w * h + w) * sizeof(_RGBQUAD), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    for (int i = 0;; i++, i %= 3) {
+        HDC desk = GetDC(NULL);
+        HDC hdcdc = CreateCompatibleDC(desk);
+        HBITMAP hbm = CreateBitmap(w, h, 1, 32, data);
+        SelectObject(hdcdc, hbm);
+		BitBlt(hdcdc, 0, 0, w, h, desk, 0, 0, SRCCOPY);
+        GetBitmapBits(hbm, w * h * 4, data);
+        for (int i = 0; i < w * h; i++) {
+			int x = i % w, y = i / h, t = y ^ y | x;
+            data[i].rgb = RGB(GetRValue(Hue(239)),GetGValue(Hue(239)),GetBValue(Hue(239)));
+		}
+        SetBitmapBits(hbm, w * h * 4, data);
+        BitBlt(desk, 0, 0, w, h, hdcdc, 0, 0, SRCCOPY);
+        DeleteObject(hbm);
+        DeleteObject(hdcdc);
+        DeleteObject(desk);
+    }
+    return 0;
+} 
+
+int main() {
+    CreateThread(0, 0, shader1, 0, 0, 0);
+    Sleep(-1);
+}
